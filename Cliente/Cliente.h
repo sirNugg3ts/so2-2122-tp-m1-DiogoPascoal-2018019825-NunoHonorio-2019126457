92bfc582906ae28353608a026f033df0ecb22508
Diogo Pascoal <diogopascoal789@gmail.com>
2022-06-17 02:24:11 +0100

    pipes everywhere

--- Cliente/Cliente.h
+++ Cliente/Cliente.h
@@ -4,11 +4,50 @@
 #pragma once
 
 #include "resource.h"
+#define NOME_PIPE TEXT("\\\\.\\pipe\\TPSO2122_SERVER_CLIENTE")
+
+//Estruturas para coms
+//Servidor -> Cliente  AKA Pedido
+typedef struct {
+    Tabuleiro infoTab;  // tabuleiro para enviar para o cliente
+    BOOL validada; //Se a jogada é valida
+}infoServidor;
+
+
+
+//Cliente -> Servidor AKA Resposta
+typedef struct {
+    TCHAR comando[256];
+    int colunaJogada; // coluna carregada
+    int linhaJogada; // linha carregada
+}infoCliente;
 
 typedef struct {
     int xPos, yPos;
     TCHAR c;
 }PosChar;
+
+typedef struct {
+    BOOL terminar;
+    infoServidor* infoServidor;
+    infoCliente* infoCliente;
+    
+}DADOS_THREAD_COMS;
+
+//Estruturas para pipes
+typedef struct NAMEDPIPE_STRUCT {
+    HANDLE hPipe; // handle do pipe
+
+    OVERLAPPED overlap;
+
+    BOOL active; //representa se a instancia do named pipe esta ou nao ativa, se ja tem um cliente ou nao
+    BOOL fPendingIO;
+    DWORD dwState;
+    TCHAR chRequest[256];
+    DWORD cbRead;
+    TCHAR chReply[256];
+    DWORD cbToWrite;
+}NAMEDPIPE_STRUCT;
 
 
 //Tabuleiro do jogo
@@ -27,20 +66,6 @@ typedef struct {
 
 
 
-//Estruturas para coms
-//Servidor -> Cliente  AKA Pedido
-typedef struct {
-    Tabuleiro infoTab;  // tabuleiro para enviar para o cliente
-    BOOL validada; //Se a jogada é valida
-}infoServidor;
 
-
-
-//Cliente -> Servidor AKA Resposta
-typedef struct {
-    TCHAR comando[256];
-    int colunaJogada; // coluna carregada
-    int linhaJogada; // linha carregada
-}infoCliente;
 
 #endif
